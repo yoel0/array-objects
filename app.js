8c682a7472b4b730ef9717efbba23879d6cf2d28
yoel <yoel@macbook-pro.lan>
2020-07-15 15:06:41 -0700

    [loops] cars array

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ console.log(myLocation);
 var favoriteNumber = "8";
 console.log(favoriteNumber);
 
-var teams =
-  ("Rockets", "Lakers", "Warriors", "Clippers", "Blazers", "Celtics", "Nets");
+var teams = [
+  "Rockets",
+  "Lakers",
+  "Warriors",
+  "Clippers",
+  "Blazers",
+  "Celtics",
+  "Nets",
+];
 console.log(teams);
 
 // while loops
@@ -45,4 +52,27 @@ var myOtherRice = 20;
 while (myOtherRice < rice) {
   console.log(myOtherRice); // 10, 11, 12
   myOtherRice += 1; // 11, 12, 13
+}
+
+// for loops
+for (var i = 0; i < 10; i++) {
+  console.log(i); // 0, 1, 2.....
+}
+
+// print teams
+for (var i = 0; i < teams.length; i++) {
+  var eachTeam = teams[i]; // Rockets, Lakers, Warriors...
+  console.log(eachTeam);
+}
+
+// Make an array
+// Use a for loop to iterate through your array
+// print each element in your array
+
+const cars = ["Tesla", "Honda", "Toyota", "Ford"];
+console.log(cars);
+
+for (let i = 0; i < cars.length; i++) {
+  let eachCar = cars[i]; // Tesla, Honda...
+  console.log(eachCar);
 }
