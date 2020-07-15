f35ed99bef7b3d98e191ff1b0a66235249b681dc
yoel <yoel@macbook-pro.lan>
2020-07-15 14:23:06 -0700

    [loops] Add while loops

--- app.js
+++ app.js
@@ -10,3 +10,21 @@ console.log(favoriteNumber);
 var teams =
   ("Rockets", "Lakers", "Warriors", "Clippers", "Blazers", "Celtics", "Nets");
 console.log(teams);
+
+// while loops
+var number = 10;
+var otherNumber = 1;
+
+while (otherNumber < number) {
+  console.log(otherNumber);
+  // otherNumber += 1;
+  otherNumber = otherNumber + 1;
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+
+while (myOtherNumber < numberTwo) {
+  console.log(myOtherNumber); // 10, 11, 12
+  myOtherNumber += 1; // 11, 12, 13
+}
