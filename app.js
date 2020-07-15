6b54a84fae0c85480d5f41aee0b373713075d7cd
yoel <yoel@macbook-pro.lan>
2020-07-15 14:03:49 -0700

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = "Yoel";
+console.log(name);
+
+var myLocation = "Los Angeles";
+console.log(myLocation);
+
+var favoriteNumber = "8";
+console.log(favoriteNumber);
+
+var teams =
+  ("Rockets", "Lakers", "Warriors", "Clippers", "Blazers", "Celtics", "Nets");
+console.log(teams);
